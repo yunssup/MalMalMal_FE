72c37d60a9babd3701e8c3e42fcd5582774701e3
yunssup <btob031541@gmail.com>
2023-08-10 00:25:35 +0900

    아이콘 css 적용부터 하기

--- src/base/Footer.jsx
+++ src/base/Footer.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import styled from "styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHome, faHeart } from "@fortawesome/free-solid-svg-icons"; // 필요한 아이콘을 import하세요
+import { faHome, faUser } from "@fortawesome/free-solid-svg-icons";
 
 const FooterContainer = styled.footer`
   text-align: center;
   padding: 10px;
   background-color: tomato;
+  position: relative;
 `;
 
 const Image = styled.img`
@@ -22,25 +23,45 @@ const EmojiButton = styled.button`
   cursor: pointer;
   color: white;
   margin: 0 5px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: absolute; /* Absolute 포지셔닝 추가 */
+  bottom: 10px; /* 아래로 10px 간격 */
+  left: 50%; /* 왼쪽 정렬 */
+  transform: translateX(-50%); /* 가운데 정렬 */
+
+  span {
+    color: #d2d2d2; /* 텍스트의 색상 */
+    margin-top: 5px; /* 텍스트와 아이콘 간의 간격 조정 */
+  }
+`;
+
+// 아이콘에 대한 스타일
+const StyledFontAwesomeIcon = styled(FontAwesomeIcon)`
+  width: 46px;
+  height: 46px;
+  color: #d2d2d2;
 `;
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   const handleEmojiClick = (emoji) => {
-    // 클릭한 이모지에 따른 동작을 여기에 작성
-    alert(`클릭한 이모지: ${emoji}`);
+    console.log("버튼");
   };
 
   return (
     <FooterContainer>
       <Image src="풋바하얀색배경.jpg" alt="로고 이미지" />
       <div>
-        <EmojiButton onClick={() => handleEmojiClick("이모지안떠")}>
-          <FontAwesomeIcon icon={faHome} />
+        <EmojiButton onClick={() => handleEmojiClick("홈으로갈거야")}>
+          <StyledFontAwesomeIcon icon={faHome} />
+          <span>홈</span>
         </EmojiButton>
-        <EmojiButton onClick={() => handleEmojiClick("하트")}>
-          <FontAwesomeIcon icon={faHeart} />
+        <EmojiButton onClick={() => handleEmojiClick("마이페이지로 갈거야")}>
+          <StyledFontAwesomeIcon icon={faUser} />
+          <span>내 계정</span>
         </EmojiButton>
       </div>
     </FooterContainer>
