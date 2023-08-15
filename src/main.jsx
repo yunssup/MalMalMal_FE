88c652daaca364f71b34505818791a6d20bda900
yunssup <btob031541@gmail.com>
2023-08-15 21:08:30 +0900

    key

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import AppRouter from "./Router"; // 변경된 파일 경로
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <AppRouter />
-  </React.StrictMode>
+  <BrowserRouter basename="Malmalmal_FE">
+    <App />
+  </BrowserRouter>
 );
